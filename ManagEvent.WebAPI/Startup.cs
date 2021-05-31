90a86c3ed8a1d3a35704f5a750b700e47b1ec6cd
Fernando <fernandolopesscruz@gmail.com>
2021-05-30 22:06:34 -0300

    Adicionado projeto Angular SPA

--- ManagEvent.WebAPI/Startup.cs
+++ ManagEvent.WebAPI/Startup.cs
@@ -31,6 +31,7 @@ namespace ManagEvent.WebAPI
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +47,8 @@ namespace ManagEvent.WebAPI
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
